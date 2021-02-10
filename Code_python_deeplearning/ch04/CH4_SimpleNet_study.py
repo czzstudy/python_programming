ffcab7d8403f2af2552974980a53d1a8576d9753
czzstudy <921767876@qq.com>
2021-02-10 16:56:00 +0800

    TwoLayerNet finished and it can work, but it is too slow

--- Code_python_deeplearning/ch04/CH4_SimpleNet_study.py
+++ Code_python_deeplearning/ch04/CH4_SimpleNet_study.py
@@ -6,6 +6,7 @@ sys.path.append("F:\study\python\python_programming\Code_python_deeplearning")
 from dataset.mnist import load_mnist
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 from common.functions import sigmoid, softmax
 
 
@@ -119,12 +120,101 @@ class simpleNet:
         loss = cross_entropy_error(y, t)
         return loss
 
+# --------简单的两层网络-------- #
+# 输入：输入输出及隐藏层权重大小，输入横向量x，实际结果t
+# 输出：预测结果predict，交叉熵损失结果loss
+# ----------------------------- #
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 权重初始化
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
 
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+        
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        return grads
 
 if __name__=='__main__':
-    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=False, flatten=True, one_hot_label=True)
-    batch_size = 10  # mini-batch大小
-    batch_mask = np.random.choice(x_train.shape[0], batch_size)  # 随机选择batch的索引
-    x_train_batch = x_train[batch_mask]
-    t_train_batch = t_train[batch_mask]
+    # 获取数据
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
     
+    # 超参数
+    iters_num = 10000
+    batch_size = 100
+    learning_rate = 0.1
+
+    # 保存过程变量
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+    iter_per_epoch = max(x_train.shape[0]/batch_size, 1) #  平均每个epoch的重复次数
+
+    network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+    for i in range(iters_num):
+        # 获取mini-batch
+        batch_mask = np.random.choice(x_train.shape[0], batch_size)  # 随机选择batch的索引
+        x_train_batch = x_train[batch_mask]
+        t_train_batch = t_train[batch_mask]
+        # 计算梯度
+        grad = network.numerical_gradient(x_train_batch, t_train_batch)
+        # 更新参数
+        for key in ('W1', 'b1', 'W2', 'b2'):
+            network.params[key] -= learning_rate*grad[key]
+        # 计算损失
+        loss = network.loss(x_train_batch, t_train_batch)
+        train_loss_list.append(loss)
+        # 计算每个epoch的识别精度
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            test_acc = network.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+            print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+    # 绘图展示中间结果
+    plt.figure(1)
+    markers = {'train': 'o', 'test': 's'}
+    x = np.arange(len(train_acc_list))
+    plt.plot(x, train_acc_list, label='train acc')
+    plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+    plt.xlabel("epochs")
+    plt.ylabel("accuracy")
+    plt.ylim(0, 1.0)
+    plt.legend(loc='lower right')
+    plt.show()
+
+    plt.figure(2)
+    plt.plot(np.arange(len(train_loss_list)), train_loss_list)
+    plt.xlabel("iter")
+    plt.ylabel("loss")
+    plt.show()
