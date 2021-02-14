c7582e8c22bf0fc0a869dc0af6bb8164dae3b7ca
czzstudy <921767876@qq.com>
2021-02-14 19:25:32 +0800

    Add the class of simple CNN

--- Code_python_deeplearning/ch07/CH7_SImpleCNN.py
+++ Code_python_deeplearning/ch07/CH7_SImpleCNN.py
@@ -5,6 +5,7 @@ from ast import NodeTransformer
 import sys
 
 from numpy.core.defchararray import not_equal
+from numpy.core.numeric import outer
 sys.path.append("F:\study\python\python_programming\Code_python_deeplearning")
 from dataset.mnist import load_mnist
 import numpy as np
@@ -14,6 +15,8 @@ from common.functions import sigmoid, softmax
 from common.gradient import numerical_gradient
 from collections import OrderedDict
 from common.util import im2col, col2im
+from common.layers import *
+import pickle
 
 # ---------最大池化层----------- #
 # 输入：初始卷积核W及偏置b，卷积核填充pad，步幅stride，四维输入x，上游梯度dout
@@ -94,6 +97,105 @@ class Pooling:
         dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
         return dx
 
+# ---------简单卷积网络----------- #
+# 网络结构：conv - relu - pool - affine - relu - affine - softmax
+# input_size : 输入大小（MNIST的情况下为784）
+# hidden_size_list : 隐藏层的神经元数量的列表（e.g. [100, 100, 100]）
+# output_size : 输出大小（MNIST的情况下为10）
+# activation : 'relu' or 'sigmoid'
+# weight_init_std : 指定权重的标准差（e.g. 0.01）
+# 指定'relu'或'he'的情况下设定“He的初始值”
+# 指定'sigmoid'或'xavier'的情况下设定“Xavier的初始值”
+# ------------------------------- #
+class SimpleConvNet:
+    def __init__(self, input_dim=(1,28,28), 
+                conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1}, 
+                hidden_size=100, output_size=10, weight_init_std=0.01):
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+        cov_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
+        pool_output_size = int(filter_num * (cov_output_size/2) * (cov_output_size/2))
+        # 权重初始化
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, ouput_size)
+        self.params['b3'] = np.zeros(output_size)
+        # 中间生成层
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'], 
+                                            conv_param['stride'], conv_param['pad'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+        # 输出层+损失函数
+        self.last_layer = SoftmaxWithLoss()
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.last_layer.forward(y, t)
+    def accuracy(self, x, t, batch_size=100):
+        if t.dim != 1:
+            t = np.argmax(t, axis=1)
+        acc = 0.0
+        for i in range(int(x.shape[0] / batch_size)):
+            tx = x[i*batch_size:(i+1)*batch_size]
+            tt = t[i*batch_size:(i+1)*batch_size]
+            y = self.predict(tx)
+            y = np.argmax(y, axis=1)
+            acc += np.sum(y == tt) 
+        return acc / x.shape[0]
+    def gradient(self, x, t):
+        """求梯度（误差反向传播法）
+        Parameters
+        ----------
+        x : 输入数据
+        t : 教师标签
+        Returns
+        -------
+        具有各层的梯度的字典变量
+            grads['W1']、grads['W2']、...是各层的权重
+            grads['b1']、grads['b2']、...是各层的偏置
+        """
+        # forward
+        self.loss(x, t)
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        # 设定
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+        return grads
+    def save_params(self, file_name="params.pkl"):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_name, 'wb') as f:
+            pickle.dump(params, f)
+    def load_params(self, file_name="params.pkl"):
+        with open(file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+        for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
+            self.layers[key].W = self.params['W' + str(i+1)]
+            self.layers[key].b = self.params['b' + str(i+1)]
 
 if __name__=='__main__':
     # 获取数据
